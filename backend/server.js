38ba54ed1350fd7dd716bf0208c75077d2093a05
unknown <muhammadehtishamraza15@gmail.com>
2025-04-25 13:01:54 +0500

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 5000;
 // const __dirname = path.resolve();
 
 app.use(express.json()); // allows us to accept json data in the req.body
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://store-app-wine-two.vercel.app",
+    credentials: true,
+  })
+);
 
 app.use("/api/products", productRoutes);
 
