dd71bd85547fcc1ac8e7a1456bd01ec77f1acdf5
unknown <muhammadehtishamraza15@gmail.com>
2025-04-25 12:53:02 +0500

    Updating.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
-import path from "path";
+// import path from "path";
 import cors from "cors";
 
 dotenv.config();
@@ -10,18 +10,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json()); // allows us to accept json data in the req.body
 app.use(cors());
 
 app.use("/api/products", productRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// });
 
 // console.log(process.env.MONGO_URI);
 
